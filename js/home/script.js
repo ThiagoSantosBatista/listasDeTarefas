cb098882120086a546530561e796b4aa56c6fb12
Thiago Batista <tsb.thiagobatista@gmail.com>
2022-10-29 15:50:47 -0300

    feat: adicionado redirecionamento ao clicar no botão sair

--- js/home/script.js
+++ js/home/script.js
@@ -1,5 +1,13 @@
 const btnMenu = document.querySelector(".nav__btn");
 const btnMenuFechar = document.querySelector(".nav__btn-fechar");
+const btnSair = document.querySelector(".sair");
+
+function logout(event) {
+  if (event.type === "touchstart") {
+    event.preventDefault();
+  }
+  window.location.href = "index.html";
+}
 
 function toggleMenu(event) {
   if (event.type === "touchstart") {
@@ -9,15 +17,17 @@ function toggleMenu(event) {
   nav.classList.toggle("ativo");
 }
 
-(function() {
-    let nome = localStorage.getItem("user");
-    let jsonData = JSON.parse(nome);
-    let userSpan = document.querySelector('.user-roxo');
+(function () {
+  let nome = localStorage.getItem("user");
+  let jsonData = JSON.parse(nome);
+  let userSpan = document.querySelector(".user-roxo");
 
-    userSpan.innerHTML = jsonData.nome;
+  userSpan.innerHTML = jsonData.nome;
 })();
 
 btnMenu.addEventListener("click", toggleMenu);
 btnMenu.addEventListener("touchstart", toggleMenu);
 btnMenuFechar.addEventListener("click", toggleMenu);
 btnMenuFechar.addEventListener("touchstart", toggleMenu);
+btnSair.addEventListener("click", logout);
+btnSair.addEventListener("touchstart", logout);
