bc4b4ae865d4140acb8f682a219680854b9fb0c5
Thiago Batista <tsb.thiagobatista@gmail.com>
2022-10-30 15:45:56 -0300

    feat: modal e função para adicionar listas criado

--- js/home/script.js
+++ js/home/script.js
@@ -1,7 +1,11 @@
 const btnMenu = document.querySelector(".nav__btn");
 const btnMenuFechar = document.querySelector(".nav__btn-fechar");
 const btnSair = document.querySelector(".sair");
-const listas = document.querySelector(".listas");
+const navListas = document.querySelector(".nav__listas");
+const btnAddLista = document.querySelector("#btnAddLista");
+const listas = document.querySelector(".main__listas");
+const addLista = document.querySelector(".adicionar");
+const btnFecharModal = document.querySelector(".fecharModal");
 
 function logout(event) {
   if (event.type === "touchstart") {
@@ -35,11 +39,61 @@ function toggleMenu(event) {
   userSpan.innerHTML = jsonData.nome;
 })();
 
+function criarLista() {
+  let inputNomeLista = document.querySelector("#inputNomeLista");
+  let nomeLista = inputNomeLista.value;
+  if (nomeLista.length > 0) {
+    let div = document.createElement("div");
+    div.classList.add("lista");
+    listas.appendChild(div);
+
+    let divListaNome = document.createElement("div");
+    divListaNome.classList.add("lista__nome");
+    div.appendChild(divListaNome);
+
+    let listaNomeH1 = document.createElement("h1");
+    listaNomeH1.innerText = nomeLista;
+    divListaNome.appendChild(listaNomeH1);
+
+    let listaNomeImg = document.createElement("img");
+    listaNomeImg.src = "./img/edit_note.svg";
+    divListaNome.appendChild(listaNomeImg);
+
+    let divListaAdd = document.createElement("div");
+    divListaAdd.classList.add("lista__add");
+    div.appendChild(divListaAdd);
+
+    let listaAddInput = document.createElement("input");
+    divListaAdd.appendChild(listaAddInput);
+
+    let listaAddBtn = document.createElement("button");
+    listaAddBtn.innerText = "add";
+    divListaAdd.appendChild(listaAddBtn);
+    inputNomeLista.value = "";
+    inputNomeLista.focus();
+  } else {
+    alert("Nome da lista deve ter no mínimo 1 caractere!");
+  }
+}
+
+function abrirModal() {
+  let modalAddLista = document.querySelector(".adicionarLista");
+  modalAddLista.classList.add("ativo");
+}
+
+function fecharModal() {
+  let modalAddLista = document.querySelector(".adicionarLista");
+  modalAddLista.classList.remove("ativo");
+}
+
 btnMenu.addEventListener("click", toggleMenu);
 btnMenu.addEventListener("touchstart", toggleMenu);
 btnMenuFechar.addEventListener("click", toggleMenu);
 btnMenuFechar.addEventListener("touchstart", toggleMenu);
 btnSair.addEventListener("click", logout);
 btnSair.addEventListener("touchstart", logout);
-listas.addEventListener("click", toggleMenu);
-listas.addEventListener("touchstart", toggleMenu);
+navListas.addEventListener("click", toggleMenu);
+navListas.addEventListener("touchstart", toggleMenu);
+addLista.addEventListener("click", criarLista);
+btnAddLista.addEventListener("click", abrirModal);
+btnFecharModal.addEventListener("click", fecharModal);
