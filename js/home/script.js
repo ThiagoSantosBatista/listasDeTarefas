52719768e1a5a26406898858d4a79ce180a1cf63
Thiago Batista <tsb.thiagobatista@gmail.com>
2022-10-30 22:45:51 -0300

    feat: salvar lista no localstorage

--- js/home/script.js
+++ js/home/script.js
@@ -3,10 +3,15 @@ const btnMenuFechar = document.querySelector(".nav__btn-fechar");
 const btnSair = document.querySelector(".sair");
 const navListas = document.querySelector(".nav__listas");
 const btnAddLista = document.querySelector("#btnAddLista");
-const listas = document.querySelector(".main__listas");
+const divListas = document.querySelector(".main__listas");
 const addLista = document.querySelector(".adicionar");
 const btnFecharModal = document.querySelector(".fecharModal");
-
+const inputNomeLista = document.querySelector("#inputNomeLista");
+const btnAddTarefa = document.querySelector(".addTarefa");
+const listas = document.getElementsByClassName(".lista")[0];
+let conteudoArrayListas = localStorage.getItem("listas")
+  ? JSON.parse(localStorage.getItem("listas"))
+  : [];
 function logout(event) {
   if (event.type === "touchstart") {
     event.preventDefault();
@@ -39,38 +44,36 @@ function toggleMenu(event) {
   userSpan.innerHTML = jsonData.nome;
 })();
 
-function criarLista() {
-  let inputNomeLista = document.querySelector("#inputNomeLista");
-  let nomeLista = inputNomeLista.value;
-  if (nomeLista.length > 0) {
-    let div = document.createElement("div");
-    div.classList.add("lista");
-    listas.appendChild(div);
+conteudoArrayListas.forEach(criarLista);
 
-    let divListaNome = document.createElement("div");
-    divListaNome.classList.add("lista__nome");
-    div.appendChild(divListaNome);
-
-    let listaNomeH1 = document.createElement("h1");
-    listaNomeH1.innerText = nomeLista;
-    divListaNome.appendChild(listaNomeH1);
-
-    let listaNomeImg = document.createElement("img");
-    listaNomeImg.src = "./img/edit_note.svg";
-    divListaNome.appendChild(listaNomeImg);
-
-    let divListaAdd = document.createElement("div");
-    divListaAdd.classList.add("lista__add");
-    div.appendChild(divListaAdd);
-
-    let listaAddInput = document.createElement("input");
-    divListaAdd.appendChild(listaAddInput);
-
-    let listaAddBtn = document.createElement("button");
-    listaAddBtn.innerText = "add";
-    divListaAdd.appendChild(listaAddBtn);
-    inputNomeLista.value = "";
-    inputNomeLista.focus();
+function criarLista(text) {
+    let conteudo = text.nome;
+  let div = document.createElement("div");
+  div.classList.add("lista");
+  div.innerHTML = `<div class="lista__nome">
+                    <h1>${conteudo}</h1>
+                    <img src="./img/edit_note.svg" alt="">
+                </div>
+                <div class="lista__add">
+                    <input class="inputTarefa" type="text">
+                    <button class="addTarefa" onclick="adicionarTarefa()">Add</button>
+                </div>
+                <ul class="lista__conteudo">
+                </ul>
+                `;
+  divListas.appendChild(div);
+  inputNomeLista.value = "";
+  inputNomeLista.focus();
+}
+function addListasDados() {
+  let value = inputNomeLista.value.trim();
+  if (value.length > 0) {
+    let listaDados = {
+      nome: inputNomeLista.value,
+    };
+    conteudoArrayListas.push(listaDados);
+    localStorage.setItem("listas", JSON.stringify(conteudoArrayListas));
+    criarLista(conteudoArrayListas[conteudoArrayListas.length - 1]);
   } else {
     alert("Nome da lista deve ter no mínimo 1 caractere!");
   }
@@ -86,6 +89,28 @@ function fecharModal() {
   modalAddLista.classList.remove("ativo");
 }
 
+function adicionarTarefa(e) {
+  const ulListaConteudo = document.querySelector(".lista__conteudo");
+  const inputTarefa = document.querySelector(".inputTarefa");
+  let value = inputTarefa.value.trim();
+  if (value.length > 0) {
+    let li = document.createElement("li");
+    li.classList.add("lista__item");
+    li.innerHTML = `<div class="checkbox-campo">
+     <input type="checkbox" id="checkbox-7">
+     <label for="checkbox-7" class="descricao"></label>
+     </div>
+     <p>${inputTarefa.value}
+     </p>
+     <img src="./img/more.svg" alt="">
+     <span class="linha"></span>`;
+    ulListaConteudo.appendChild(li);
+    inputTarefa.value = "";
+  } else {
+    alert("Escreva uma tarefa!");
+  }
+}
+
 btnMenu.addEventListener("click", toggleMenu);
 btnMenu.addEventListener("touchstart", toggleMenu);
 btnMenuFechar.addEventListener("click", toggleMenu);
@@ -94,6 +119,11 @@ btnSair.addEventListener("click", logout);
 btnSair.addEventListener("touchstart", logout);
 navListas.addEventListener("click", toggleMenu);
 navListas.addEventListener("touchstart", toggleMenu);
-addLista.addEventListener("click", criarLista);
+addLista.addEventListener("click", addListasDados);
 btnAddLista.addEventListener("click", abrirModal);
 btnFecharModal.addEventListener("click", fecharModal);
+document.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    addLista.click();
+  }
+});
