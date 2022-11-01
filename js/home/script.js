d00afd50b2c78a0b7f5a1ef5aa6ea7d07a24c140
Thiago Batista <tsb.thiagobatista@gmail.com>
2022-10-31 23:43:28 -0300

    feat: deixando as tarefas salvas no localstorage

--- js/home/script.js
+++ js/home/script.js
@@ -7,9 +7,27 @@ const divListas = document.querySelector(".main__listas");
 const addLista = document.querySelector(".adicionar");
 const btnFecharModal = document.querySelector(".fecharModal");
 const inputNomeLista = document.querySelector("#inputNomeLista");
-const listas = document.getElementsByClassName(".lista")[0];
 let conteudoArrayListas = localStorage.getItem("listas")
   ? JSON.parse(localStorage.getItem("listas"))
+  : [];
+
+let tarefasLista0 = localStorage.getItem("lista0")
+  ? JSON.parse(localStorage.getItem("lista0"))
+  : [];
+let tarefasLista1 = localStorage.getItem("lista1")
+  ? JSON.parse(localStorage.getItem("lista1"))
+  : [];
+let tarefasLista2 = localStorage.getItem("lista2")
+  ? JSON.parse(localStorage.getItem("lista2"))
+  : [];
+let tarefasLista3 = localStorage.getItem("lista3")
+  ? JSON.parse(localStorage.getItem("lista3"))
+  : [];
+let tarefasLista4 = localStorage.getItem("lista4")
+  ? JSON.parse(localStorage.getItem("lista4"))
+  : [];
+let tarefasLista5 = localStorage.getItem("lista5")
+  ? JSON.parse(localStorage.getItem("lista5"))
   : [];
 
 function logout(event) {
@@ -64,6 +82,97 @@ conteudoArrayListas.forEach((text, id) => {
   inputNomeLista.focus();
 });
 
+tarefasLista0.forEach((value, id) => {
+  let tarefa = value.tarefa;
+  const ulListaConteudo = document.querySelector(".lc0");
+  let li = document.createElement("li");
+  li.classList.add("lista__item");
+  li.innerHTML = `<div class="checkbox-campo">
+       <input type="checkbox" id="checkbox-${id}">
+       <label for="checkbox-${id}" class="descricao"></label>
+       </div>
+       <p>${tarefa}
+       </p>
+       <img src="./img/more.svg" alt="">
+       <span class="linha"></span>`;
+  ulListaConteudo.appendChild(li);
+});
+tarefasLista1.forEach((value, id) => {
+  let tarefa = value.tarefa;
+  const ulListaConteudo = document.querySelector(".lc1");
+  let li = document.createElement("li");
+  li.classList.add("lista__item");
+  li.innerHTML = `<div class="checkbox-campo">
+         <input type="checkbox" id="checkbox-${id}">
+         <label for="checkbox-${id}" class="descricao"></label>
+         </div>
+         <p>${tarefa}
+         </p>
+         <img src="./img/more.svg" alt="">
+         <span class="linha"></span>`;
+  ulListaConteudo.appendChild(li);
+});
+tarefasLista2.forEach((value, id) => {
+  let tarefa = value.tarefa;
+  const ulListaConteudo = document.querySelector(".lc2");
+  let li = document.createElement("li");
+  li.classList.add("lista__item");
+  li.innerHTML = `<div class="checkbox-campo">
+         <input type="checkbox" id="checkbox-${id}">
+         <label for="checkbox-${id}" class="descricao"></label>
+         </div>
+         <p>${tarefa}
+         </p>
+         <img src="./img/more.svg" alt="">
+         <span class="linha"></span>`;
+  ulListaConteudo.appendChild(li);
+});
+tarefasLista3.forEach((value, id) => {
+  let tarefa = value.tarefa;
+  const ulListaConteudo = document.querySelector(".lc3");
+  let li = document.createElement("li");
+  li.classList.add("lista__item");
+  li.innerHTML = `<div class="checkbox-campo">
+         <input type="checkbox" id="checkbox-${id}">
+         <label for="checkbox-${id}" class="descricao"></label>
+         </div>
+         <p>${tarefa}
+         </p>
+         <img src="./img/more.svg" alt="">
+         <span class="linha"></span>`;
+  ulListaConteudo.appendChild(li);
+});
+tarefasLista4.forEach((value, id) => {
+  let tarefa = value.tarefa;
+  const ulListaConteudo = document.querySelector(".lc4");
+  let li = document.createElement("li");
+  li.classList.add("lista__item");
+  li.innerHTML = `<div class="checkbox-campo">
+         <input type="checkbox" id="checkbox-${id}">
+         <label for="checkbox-${id}" class="descricao"></label>
+         </div>
+         <p>${tarefa}
+         </p>
+         <img src="./img/more.svg" alt="">
+         <span class="linha"></span>`;
+  ulListaConteudo.appendChild(li);
+});
+tarefasLista5.forEach((value, id) => {
+  let tarefa = value.tarefa;
+  const ulListaConteudo = document.querySelector(".lc5");
+  let li = document.createElement("li");
+  li.classList.add("lista__item");
+  li.innerHTML = `<div class="checkbox-campo">
+         <input type="checkbox" id="checkbox-${id}">
+         <label for="checkbox-${id}" class="descricao"></label>
+         </div>
+         <p>${tarefa}
+         </p>
+         <img src="./img/more.svg" alt="">
+         <span class="linha"></span>`;
+  ulListaConteudo.appendChild(li);
+});
+
 conteudoArrayListas.forEach((undefined, id) => {
   let btnAddTarefa = document.querySelector(`.at${id}`);
   const ulListaConteudo = document.querySelector(`.lc${id}`);
@@ -82,6 +191,44 @@ conteudoArrayListas.forEach((undefined, id) => {
        <img src="./img/more.svg" alt="">
        <span class="linha"></span>`;
       ulListaConteudo.appendChild(li);
+      if (id === 0) {
+        let listaTarefa = {
+          tarefa: inputTarefa.value,
+        };
+        tarefasLista0.push(listaTarefa);
+        localStorage.setItem("lista0", JSON.stringify(tarefasLista0));
+      } else if (id === 1) {
+        let listaTarefa = {
+          tarefa: inputTarefa.value,
+        };
+        tarefasLista1.push(listaTarefa);
+        localStorage.setItem("lista1", JSON.stringify(tarefasLista1));
+      } else if (id === 2) {
+        let listaTarefa = {
+          tarefa: inputTarefa.value,
+        };
+        tarefasLista2.push(listaTarefa);
+        localStorage.setItem("lista2", JSON.stringify(tarefasLista2));
+      } else if (id === 3) {
+        let listaTarefa = {
+          tarefa: inputTarefa.value,
+        };
+        tarefasLista3.push(listaTarefa);
+        localStorage.setItem("lista3", JSON.stringify(tarefasLista3));
+      } else if (id === 4) {
+        let listaTarefa = {
+          tarefa: inputTarefa.value,
+        };
+        tarefasLista4.push(listaTarefa);
+        localStorage.setItem("lista4", JSON.stringify(tarefasLista4));
+      } else if (id === 5) {
+        let listaTarefa = {
+          tarefa: inputTarefa.value,
+        };
+        tarefasLista5.push(listaTarefa);
+        localStorage.setItem("lista5", JSON.stringify(tarefasLista5));
+      }
+
       inputTarefa.value = "";
     } else {
       alert("Escreva uma tarefa!");
@@ -130,6 +277,44 @@ function criarTarefa() {
        <img src="./img/more.svg" alt="">
        <span class="linha"></span>`;
       ulListaConteudo.appendChild(li);
+      if (id === 0) {
+        let listaTarefa = {
+          tarefa: inputTarefa.value,
+        };
+        tarefasLista0.push(listaTarefa);
+        localStorage.setItem("lista0", JSON.stringify(tarefasLista0));
+      } else if (id === 1) {
+        let listaTarefa = {
+          tarefa: inputTarefa.value,
+        };
+        tarefasLista1.push(listaTarefa);
+        localStorage.setItem("lista1", JSON.stringify(tarefasLista1));
+      } else if (id === 2) {
+        let listaTarefa = {
+          tarefa: inputTarefa.value,
+        };
+        tarefasLista2.push(listaTarefa);
+        localStorage.setItem("lista2", JSON.stringify(tarefasLista2));
+      } else if (id === 3) {
+        let listaTarefa = {
+          tarefa: inputTarefa.value,
+        };
+        tarefasLista3.push(listaTarefa);
+        localStorage.setItem("lista3", JSON.stringify(tarefasLista3));
+      } else if (id === 4) {
+        let listaTarefa = {
+          tarefa: inputTarefa.value,
+        };
+        tarefasLista4.push(listaTarefa);
+        localStorage.setItem("lista4", JSON.stringify(tarefasLista4));
+      } else if (id === 5) {
+        let listaTarefa = {
+          tarefa: inputTarefa.value,
+        };
+        tarefasLista5.push(listaTarefa);
+        localStorage.setItem("lista5", JSON.stringify(tarefasLista5));
+      }
+
       inputTarefa.value = "";
     } else {
       alert("Escreva uma tarefa!");
@@ -140,6 +325,7 @@ function criarTarefa() {
 
 function addListasDados() {
   let value = inputNomeLista.value.trim();
+  if (conteudoArrayListas.length > 5) return alert("Máximo de 6 listas");
   if (value.length > 0) {
     let listaDados = {
       nome: inputNomeLista.value,
@@ -149,7 +335,9 @@ function addListasDados() {
     criarLista(conteudoArrayListas[conteudoArrayListas.length - 1]);
     criarTarefa(conteudoArrayListas[conteudoArrayListas.length - 1]);
   } else {
-    alert("Nome da lista deve ter no mínimo 1 caractere!");
+    alert(
+      "Nome da lista deve ter no mínimo 1 caractere! Obs: Máximo de 6 listas."
+    );
   }
 }
 
